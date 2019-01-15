5e12830c98182330fad2229ca647ae8be74c083d
Hugo Taillefumier <htaillef@z1r7p2.le-101.fr>
2019-01-15 17:06:08 +0100

    Params are set in struct chmp, PC is up after read instructions

---
+++ Corewar_htaillef/srcs_cor/main.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   main.c                                           .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: gzanarel <gzanarel@student.le-101.fr>      +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/01/09 15:04:49 by gzanarel     #+#   ##    ##    #+#       */
+/*   Updated: 2019/01/15 15:53:50 by gzanarel    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "../include/corewar.h"
+
+
+void	check_infos(t_cor *c)
+{
+	int i;
+
+	i = -1;
+	while (++i < c->vm->nb_player)
+	{
+		if (c->chmp[i]->champ_size > 682)
+			ft_exit(3);
+	}
+}
+
+int		main(int ac, char **av)
+{
+	t_cor *c;
+	char *line;
+	unsigned int len;
+
+	c = ft_memalloc(sizeof(t_cor));
+	init_vm(c, av, ac);
+	read_infos(c, av);
+	check_infos(c);
+	init_map(c);
+	exec_process(c->vm, c->chmp[0]);
+	//print_map(c, 64);
+	cycle(c);
+	return (0);
+}
