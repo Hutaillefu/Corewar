dab75c14793a6857bcaab6607b33fd1ea4fb725e
Gabriel Zanarelli <gzanarel@z1r6p2.le-101.fr>
2019-01-17 18:20:51 +0100

    allo

--- Corewar_gzana/srcs_cor/init.c
+++ Corewar_gzana/srcs_cor/init.c
@@ -6,7 +6,7 @@
 /*   By: gzanarel <gzanarel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/01/10 11:21:38 by gzanarel     #+#   ##    ##    #+#       */
-/*   Updated: 2019/01/17 15:36:08 by gzanarel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/01/17 17:38:47 by gzanarel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -23,8 +23,37 @@ void	init_map(t_cor *c)
 	start = MEM_SIZE / c->vm->nb_player;
 	while (++player < c->vm->nb_player)
 	{
-		ft_printf("Player %d, weighing %u bytes, \"%s\" (%s) !\n", (player + 1), c->chmp[player]->champ_size, c->chmp[player]->name, c->chmp[player]->comment);
+		ft_printf("* Player %d (nb: %X(%d)), weighing %u bytes, \"%s\" (%s) !\n", (player + 1), c->chmp[player]->num, c->chmp[player]->num, c->chmp[player]->champ_size, c->chmp[player]->name, c->chmp[player]->comment);
 		ft_memsub(c->vm->area, c->chmp[player]->infos, (start * player), c->chmp[player]->champ_size);
+	}
+}
+
+void	init_num_chmp(t_cor *c, int i)
+{
+	int j;
+
+	j = -1;
+	if (c->vm->num[i] >= 0)
+	{
+		while (++j < i)
+			if (c->chmp[j]->num == -c->vm->num[i])
+			{
+				c->vm->num[i] += 1;
+				j = -1;
+			}
+		c->chmp[i]->reg[0] = c->vm->num[i] * -1;
+		c->chmp[i]->num = c->vm->num[i] * -1;
+	}
+	else
+	{
+		c->chmp[i]->reg[0] = (i + 1) * -1;
+		while (++j < i)
+			if (c->chmp[j]->num == c->chmp[i]->reg[0])
+			{
+				c->chmp[i]->reg[0] -= 1;
+				j = -1;
+			}
+		c->chmp[i]->num = c->chmp[i]->reg[0];
 	}
 }
 
@@ -36,7 +65,7 @@ void	init_chmp(t_cor *c)
 	i = -1;
 	while (++i < c->vm->nb_player)
 	{
-		j = -1;
+		j = 0;
 		c->chmp[i] = malloc(sizeof(t_chmp));
 		c->chmp[i]->champ_size = 0;
 		c->chmp[i]->name = NULL;
@@ -46,17 +75,20 @@ void	init_chmp(t_cor *c)
 		c->chmp[i]->pc_b = c->chmp[i]->pc;
 		c->chmp[i]->exec = 0;
 		c->chmp[i]->lives = 0;
+		c->chmp[i]->last_live = 0;
 		c->chmp[i]->op_size = 0;
 		while (++j < REG_NUMBER)
 			c->chmp[i]->reg[j] = 0;
-		c->chmp[i]->reg[0] = (i + 1) * -1;
-		c->chmp[i]->num = c->chmp[i]->reg[0];
+		init_num_chmp(c, i);
 		c->chmp[i]->carry = 0;
 	}
 }
 
 void	init_vm(t_cor *c, char **av, int ac)
 {
+	int j;
+
+	j = -1;
 	c->vm = ft_memalloc(sizeof(t_vm));
 	c->vm->cycle_delta = CYCLE_DELTA;
 	c->cycle_to_die = 1536;
@@ -70,6 +102,8 @@ void	init_vm(t_cor *c, char **av, int ac)
 	c->vm->champ_msize = CHAMP_MAX_SIZE;
 	c->vm->cycle = 0;
 	c->vm->dump = -1;
+	while (++j < MAX_PLAYERS)
+		c->vm->num[j] = -1;
 	if (check_parse(c, av, ac) == 1)
 		ft_exit(1);
 	init_chmp(c);
--- Corewar_gzana/srcs_cor/instructions.c
+++ Corewar_gzana/srcs_cor/instructions.c
@@ -6,7 +6,7 @@
 /*   By: gzanarel <gzanarel@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/01/11 13:33:40 by htaillef     #+#   ##    ##    #+#       */
-/*   Updated: 2019/01/17 15:30:15 by gzanarel    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/01/17 18:20:28 by gzanarel    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -40,14 +40,23 @@ void	write_uint(t_vm *vm, int value, int start_index, int bytes_len)
   ** dir -> param
   ** ind -> read_next_uint(vm, chmp->pc + param, 4)
 */
-int		get_param_value(t_vm *vm, t_chmp *chmp, int param, int type)
+int		get_param_value(t_vm *vm, t_chmp *chmp, int param[2], int mod)
 {
-	if (type == DIR_CODE)
-		return (param);
-	else if (type == REG_CODE)
-		return (chmp->reg[param - 1]);
-	else if (type == IND_CODE)
-		return (read_next_uint(vm, chmp->pc + param /* Modulo ? */, 4));
+	int addr;
+
+	if (param[1] == DIR_CODE)
+		return (param[0]);
+	else if (param[1] == REG_CODE)
+		return (chmp->reg[param[0] - 1]);
+	else if (param[1] == IND_CODE)
+	{
+		if (mod)
+			addr = (chmp->pc + param[0]) % (chmp->pc_b + IDX_MOD);
+		else 
+			addr = (chmp->pc + param[0]) % MEM_SIZE;
+		addr = addr < 0 ? MEM_SIZE -(-addr) : addr;
+		return (read_next_uint(vm, addr, 4));
+	}
 	return (-1);
 }
 
@@ -76,6 +85,7 @@ void	i_live(t_chmp *chmp, t_cor *cor)
 	if (!(selected_chmp = get_chmp_by_num(cor, champ_num)))
 		return ;
 	selected_chmp->lives++;
+	selected_chmp->last_live = cor->vm->cycle;
 }
 
 // Should be ok
@@ -86,9 +96,9 @@ void	i_sti(t_chmp *chmp, t_vm *vm)
 	int p2;
 	int	p3;
 	addr = 0;
-	p1 = get_param_value(vm, chmp, chmp->param[1][0], chmp->param[1][1]);
-	p2 = get_param_value(vm, chmp, chmp->param[2][0], chmp->param[2][1]);
-	p3 = get_param_value(vm, chmp, chmp->param[0][0], chmp->param[0][1]);
+	p1 = get_param_value(vm, chmp, chmp->param[1], 1);
+	p2 = get_param_value(vm, chmp, chmp->param[2], 1);
+	p3 = get_param_value(vm, chmp, chmp->param[0], 1);
 	addr = (chmp->pc + p1 + p2) % (chmp->pc_b + IDX_MOD);
 	addr = addr < 0 ? MEM_SIZE -(-addr) : addr;
 	write_uint(vm, p3, addr, REG_SIZE);
@@ -101,8 +111,8 @@ void	i_ldi(t_chmp *chmp, t_vm *vm)
 	int p2;
 	int addr;
 
-	p1 = get_param_value(vm, chmp, chmp->param[0][0], chmp->param[0][1]);
-	p2 = get_param_value(vm, chmp, chmp->param[1][0], chmp->param[1][1]);
+	p1 = get_param_value(vm, chmp, chmp->param[0], 1);
+	p2 = get_param_value(vm, chmp, chmp->param[1], 1);
 	addr = (chmp->pc + p1 + p2) % (chmp->pc_b + IDX_MOD);
 	addr = addr < 0 ? MEM_SIZE -(-addr) : addr;
 	chmp->reg[chmp->param[2][0] - 1] = read_next_uint(vm, addr, REG_SIZE);
@@ -113,8 +123,8 @@ void	i_lldi(t_chmp *chmp, t_vm *vm)
 	int p1;
 	int p2;
 
-	p1 = get_param_value(vm, chmp, chmp->param[0][0], chmp->param[0][1]);
-	p2 = get_param_value(vm, chmp, chmp->param[1][0], chmp->param[1][1]);
+	p1 = get_param_value(vm, chmp, chmp->param[0], 0);
+	p2 = get_param_value(vm, chmp, chmp->param[1], 0);
 	chmp->reg[chmp->param[2][0] - 1] = read_next_uint(vm, (chmp->pc + p1 + p2) % MEM_SIZE, REG_SIZE);
 }
 
@@ -124,8 +134,8 @@ void	i_add(t_chmp *chmp, t_vm *vm)
 	int p1;
 	int p2;
 
-	p1 = get_param_value(vm, chmp, chmp->param[0][0], chmp->param[0][1]);
-	p2 = get_param_value(vm, chmp, chmp->param[1][0], chmp->param[1][1]);
+	p1 = get_param_value(vm, chmp, chmp->param[0], 1);
+	p2 = get_param_value(vm, chmp, chmp->param[1], 1);
 	chmp->reg[chmp->param[2][0] - 1] = p1 + p2;
 	//chmp->carry = chmp->reg[chmp->param[2][0] - 1] == 0; How to manage carry ?
 }
@@ -136,8 +146,8 @@ void	i_sub(t_chmp *chmp, t_vm *vm)
 	int p1;
 	int p2;
 
-	p1 = get_param_value(vm, chmp, chmp->param[0][0], chmp->param[0][1]);
-	p2 = get_param_value(vm, chmp, chmp->param[1][0], chmp->param[1][1]);
+	p1 = get_param_value(vm, chmp, chmp->param[0], 1);
+	p2 = get_param_value(vm, chmp, chmp->param[1], 1);
 	chmp->reg[chmp->param[2][0] - 1] = p1 - p2;
 }
 
@@ -147,8 +157,8 @@ void	i_and(t_chmp *chmp, t_vm *vm)
 	int p1;
 	int p2;
 
-	p1 = get_param_value(vm, chmp, chmp->param[0][0], chmp->param[0][1]);
-	p2 = get_param_value(vm, chmp, chmp->param[1][0], chmp->param[1][1]);
+	p1 = get_param_value(vm, chmp, chmp->param[0], 1);
+	p2 = get_param_value(vm, chmp, chmp->param[1], 1);
 	chmp->reg[chmp->param[2][0] - 1] = p1 & p2;
 }
 
@@ -158,8 +168,8 @@ void	i_or(t_chmp *chmp, t_vm *vm)
 	int p1;
 	int p2;
 
-	p1 = get_param_value(vm, chmp, chmp->param[0][0], chmp->param[0][1]);
-	p2 = get_param_value(vm, chmp, chmp->param[1][0], chmp->param[1][1]);
+	p1 = get_param_value(vm, chmp, chmp->param[0], 1);
+	p2 = get_param_value(vm, chmp, chmp->param[1], 1);
 	chmp->reg[chmp->param[2][0] - 1] = p1 | p2;
 }
 
@@ -169,17 +179,26 @@ void	i_xor(t_chmp *chmp, t_vm *vm)
 	int p1;
 	int p2;
 
-	p1 = get_param_value(vm, chmp, chmp->param[0][0], chmp->param[0][1]);
-	p2 = get_param_value(vm, chmp, chmp->param[1][0], chmp->param[1][1]);
+	p1 = get_param_value(vm, chmp, chmp->param[0], 1);
+	p2 = get_param_value(vm, chmp, chmp->param[1], 1);
 	chmp->reg[chmp->param[2][0] - 1] = p1 ^ p2;
 }
 
-// MIss % IDX_MOD but dont know where to put
 void	i_ld(t_chmp *chmp, t_vm *vm)
 {
 	int p1;
 
-	p1 = get_param_value(vm, chmp, chmp->param[0][0], chmp->param[0][1]);
+	printf("Execute ld\n");
+	p1 = get_param_value(vm, chmp, chmp->param[0], 1);
+	printf("LD param is %d\n", p1);
+	chmp->reg[chmp->param[1][0] - 1] = p1;
+}
+
+void	i_lld(t_chmp *chmp, t_vm *vm)
+{
+	int p1;
+
+	p1 = get_param_value(vm, chmp, chmp->param[0], 0);
 	chmp->reg[chmp->param[1][0] - 1] = p1;
 }
 
@@ -191,6 +210,6 @@ char	i_aff(t_chmp *chmp, t_vm *vm)
 {
 	int p1;
 
-	p1 = get_param_value(vm, chmp, chmp->param[0][0], chmp->param[0][1]);
+	p1 = get_param_value(vm, chmp, chmp->param[0], 1);
 	return ((unsigned char)(p1 % 256));
 }
